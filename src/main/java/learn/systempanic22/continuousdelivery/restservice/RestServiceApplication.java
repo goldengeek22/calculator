69a5860f1760e1a24829ab20950c6e4e69a6d005
Alexandre AMEVOR <amevoralexandre@gmail.com>
2023-07-12 00:55:25 +0000

    some changes to test triggger by polling scm

--- src/main/java/learn/systempanic22/continuousdelivery/restservice/RestServiceApplication.java
+++ src/main/java/learn/systempanic22/continuousdelivery/restservice/RestServiceApplication.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestServiceApplication {
 
-    //private static final String constant = "constant";
+    private static final String AUTHOR = "Alexandre";
 
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
